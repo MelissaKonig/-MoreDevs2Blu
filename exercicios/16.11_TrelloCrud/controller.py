a81cfdbfe737e3d59e8b5e9bd7a5be70ff4710eb
Melissa Konig <melissa.konig@proway.treina>
2022-11-21 19:44:31 -0300

    Atualizar

--- exercicios/16.11_TrelloCrud/controller.py
+++ exercicios/16.11_TrelloCrud/controller.py
@@ -20,5 +20,41 @@ def read():
         conta.saldo = conta__objeto[2]
         lista_contas.append(conta)
         
+        lista_contas.append(conta)
     contas.close()
     return lista_contas
+
+def update(conta_update:Conta):
+    lista_contas = []
+    contas = open('contas.txt', 'r')
+    
+    for conta in contas:
+        conta_limpa = conta.strip()
+        conta_objeto = conta_limpa.split(';')
+        
+        if conta_update.numero == int(conta_objeto[1]):
+            lista_contas.append(str(conta_update)+'\n')
+        else:
+            lista_contas.append(conta)
+            
+    contas.close()
+    
+    contas = open('contas.txt', 'w')
+    contas.writelines(lista_contas)
+    contas.close()
+    
+def delete(numero_conta):
+    lista_contas = []
+    contas = open('contas.txt', 'r')
+    
+    for conta in contas:
+        conta_limpa = conta.strip()
+        conta_objeto = conta_limpa.split(';')
+        
+        if numero_conta != int(conta_objeto[1]):
+            lista_contas.append(conta)
+        contas.close()
+        
+        contas = open('contas.txt', 'w')
+        contas.writelines(lista_contas)
+        contas.close()
